43f2613f972d79a3f1563b1e8cdcc2e503359e19
Mateus Moreira <mateus.silva16@estudante.ifms.edu.br>
2026-08-14 18:20:58 +0000

    edit: alteração no status code e mensagem de resposta

--- sevidor-http-nativo/server.js
+++ sevidor-http-nativo/server.js
@@ -1,4 +1,4 @@
-const http = require('node:http')
+import http from 'node:http'
 
 const porta = 3000
 
@@ -7,9 +7,9 @@ const server = http.createServer();
 server.on('request', (req, res) => {
     console.log(`Servidor funcionando! ${req.method} ${req.url}`);
 
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/plain; charset=utf=8');
-    res.end("Servidor Funcionando!")
+    res.statusCode = 201
+    res.setHeader('Content-type', 'text/plain', 'charset: utf-8');
+    res.end("Recurso criado")
 });
 
 server.listen(porta, () => {
