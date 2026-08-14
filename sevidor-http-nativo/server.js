286bc2cefda428c9eaea2ee0d41197d85d3990ae
Mateus Moreira <mateus.silva16@estudante.ifms.edu.br>
2026-08-14 18:28:07 +0000

    add:  horario das requisicoes

--- sevidor-http-nativo/server.js
+++ sevidor-http-nativo/server.js
@@ -4,8 +4,12 @@ const porta = 3000
 
 const server = http.createServer();
 
+const hora = new Date().toISOString()
+
 server.on('request', (req, res) => {
     console.log(`Servidor funcionando! ${req.method} ${req.url}`);
+    console.log(`no horário ${hora}`);
+    
 
     res.statusCode = 201
     res.setHeader('Content-type', 'text/plain', 'charset: utf-8');
